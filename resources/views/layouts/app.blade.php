ec815c4c2351bcaa41f1f85a606769b09c0445ad
GaiusIsaac00x <isaacgaius1999@gmail.com>
2024-01-15 17:39:43 +0100

    workesr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,8 @@
             @extends('welcome')
 @section('body')
  <!-- Main Content-->
+
+ 
  <div class="container px-4 px-lg-5">
     <div class="row gx-4 gx-lg-5 justify-content-center">
         <div class="col-md-10 col-lg-8 col-xl-7">
